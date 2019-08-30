69f2b31de5c74722ebbbfc7cc1768000c531f48a
nakama <nakama@202719.local>
2019-08-30 18:02:50 +0700

    nothing

---
+++ app/src/main/java/com/devcamp/devcamp_mobile/module/Register/RegisterToko.kt
@@ -0,0 +1,11 @@
+package com.devcamp.devcamp_mobile.module.Register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterToko : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
