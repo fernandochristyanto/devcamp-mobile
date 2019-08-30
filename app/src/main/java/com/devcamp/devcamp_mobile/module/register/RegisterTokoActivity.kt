71f955a0002ae9eecbba119da16f8756f37c4b44
nakama <nakama@202719.local>
2019-08-30 22:25:14 +0700

    set session after form register submitted

--- app/src/main/java/com/devcamp/devcamp_mobile/module/register/RegisterTokoActivity.kt
+++ app/src/main/java/com/devcamp/devcamp_mobile/module/register/RegisterTokoActivity.kt
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.devcamp.devcamp_mobile.R
+import com.devcamp.devcamp_mobile.common.User
 import com.devcamp.devcamp_mobile.common.dto.ShopRegistrationDTO
+import com.devcamp.devcamp_mobile.data.UserSession
 import com.devcamp.devcamp_mobile.module.Register.RegisterTokoViewModel
 import com.devcamp.devcamp_mobile.module.login.LoginViewModel
 import com.google.gson.annotations.SerializedName
@@ -26,29 +28,23 @@ class RegisterTokoActivity : AppCompatActivity() {
 
 
         var status:String?=null;
+
         var statusDTO:ShopRegistrationDTO = ShopRegistrationDTO(name,location,phoneNumber,email,password,confirmPassword)
         val viewModel = ViewModelProviders.of(this).get(RegisterTokoViewModel::class.java)
         viewModel.getStatusRegistration(statusDTO)
+        val session=UserSession
         viewModel.shopStatus.observe(this, Observer {
-            status=it.status
-            Toast.makeText(LoginActivity@this, it.status, Toast.LENGTH_SHORT).show()
+            session.setUser(User(it.userID,it.email,it.password))
         })
-        if(status!=null){
-            if(status.equals("success",true)){
-                //will be implemented
 
-            }
-            else{
-                Toast.makeText(this,"Fail register",Toast.LENGTH_SHORT).show()
-            }
 
-        }
+
 
 
 
 //        @SerializedName("name") var name: String,
 //        @SerializedName("location") var location: String,
-//        @SerializedName("phone_number") var phone_number: String,
+//        @SerializedName("pho   ne_number") var phone_number: String,
 //        @SerializedName("email") var email: String?,
 //        @SerializedName("password") var password: String?,
 //        @SerializedName("user_id") var user_id: String?
