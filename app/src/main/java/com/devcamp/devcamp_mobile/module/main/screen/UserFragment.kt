c64a6ee7f30b3ece33d0186bf82b03e84e82aa95
Fernando Christyanto <christyantofernando@gmail.com>
2019-08-30 20:49:21 +0700

    Polish ui

--- app/src/main/java/com/devcamp/devcamp_mobile/module/main/screen/UserFragment.kt
+++ app/src/main/java/com/devcamp/devcamp_mobile/module/main/screen/UserFragment.kt
@@ -1,15 +1,26 @@
 package com.devcamp.devcamp_mobile.module.main.screen
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.devcamp.devcamp_mobile.R
+import com.devcamp.devcamp_mobile.module.my_products.MyProductsActivity
 
 class UserFragment : Fragment(){
+    lateinit var btnGarageSale: Button
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_userprofile, container, false)
+        btnGarageSale = view.findViewById(R.id.btnMyGarageSales)
+
+        btnGarageSale.setOnClickListener {
+            val intent = Intent(context, MyProductsActivity::class.java)
+            context?.startActivity(intent)
+        }
 
         return view
     }
